34250d73b0555e623468c58a6cd83b9ce13d34de
Simon Liebelt <si.liebelt@gmail.com>
2020-10-23 10:33:26 +0200

    Create main window and implement simple function

---
+++ doc/cheat_sheet.rst
@@ -0,0 +1,9 @@
+PyQt5
+#####
+
+QtDesigner ui to Python file
+   - Go to \*.ui file
+   - Open Cmd and type:
+   - pyuic5 -x .\mainwindow.ui -o test.py
+   - "-x": This will add python code to execute the generated file.
+   - "-o": Define name of output file.
--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-from tkinter import filedialog
-from tkinter import *
-
-def browse_button():
-    # Allow user to select a directory and store it in global var
-    # called folder_path
-    global folder_path
-    filename = filedialog.askdirectory()
-    folder_path.set(filename)
-    print(filename)
+import mainWindow
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-root = Tk()
-folder_path = StringVar()
-lbl1 = Label(master=root,textvariable=folder_path)
-lbl1.grid(row=0, column=1)
-button2 = Button(text="Browse", command=browse_button)
-button2.grid(row=0, column=3)
+def add_directory():
+    filename = QtWidgets.QFileDialog.getExistingDirectory()
+    ui.listDir.addItem(str(filename))
 
-mainloop()
+
+app = QtWidgets.QApplication(sys.argv)
+mWin = QtWidgets.QMainWindow()
+ui = mainWindow.Ui_MainWindow()
+ui.setupUi(mWin)
+#mWin = uic.loadUi("mainwindow.ui")
+ui.btnAddDir.clicked.connect(add_directory)
+
+mWin.show()
+sys.exit(app.exec_())
